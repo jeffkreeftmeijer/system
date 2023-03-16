9d06e213b955ad49f4a5867b6850bfde3f4f15a8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-16 11:06:40 +0100

    Add emacs-overlay

--- flake.nix
+++ flake.nix
@@ -7,12 +7,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, emacs-overlay, ... }:
     let
       system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system}.extend(emacs-overlay.overlay);
     in {
       homeConfigurations.jeffkreeftmeijer = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
