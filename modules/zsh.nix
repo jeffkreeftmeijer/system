a71363f164bb79d01c2c64c707471fd7e7abe28b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-16 13:01:59 +0100

    Prefix configuration options

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -2,17 +2,13 @@
 
 {
   home.packages = [ pkgs.git ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = false;
-    historySubstringSearch.enable = true;
-    initExtra =
-    ''
-    source ~/.nix-profile/share/git/contrib/completion/git-prompt.sh
-    setopt PROMPT_SUBST
-    export PS1='%~ $(__git_ps1 "(%s) ")%# '
-    '';
-    home.packages = [ pkgs.git ];
-  };
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = false;
+  programs.zsh.historySubstringSearch.enable = true;
+  programs.zsh.initExtra =
+  ''
+  source ~/.nix-profile/share/git/contrib/completion/git-prompt.sh
+  setopt PROMPT_SUBST
+  export PS1='%~ $(__git_ps1 "(%s) ")%# '
+  '';
 }
