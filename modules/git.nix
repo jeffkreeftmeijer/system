e433f89dd190c2b4a3f373be369f1316e71ce9b9
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-16 09:36:48 +0100

    Prefix configuration options

--- modules/git.nix
+++ modules/git.nix
@@ -1,9 +1,7 @@
 {
-  programs.git = {
-    enable = true;
-    userName = "Jeff Kreeftmeijer";
-    userEmail = "jeffkreeftmeijer@gmail.com";
-    extraConfig.init.defaultBranch = "main";
-    ignores = ["DS_Store"];
-  };
+  programs.git.enable = true;
+  programs.git.userName = "Jeff Kreeftmeijer";
+  programs.git.userEmail = "jeffkreeftmeijer@gmail.com";
+  programs.git.extraConfig.init.defaultBranch = "main";
+  programs.git.ignores = ["DS_Store"];
 }
