89ac73f4d84d0a0cdaaa5c06243b0d97bc50d4f5
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-19 00:48:07 +0100

    Update git email

--- modules/git.nix
+++ modules/git.nix
@@ -1,7 +1,7 @@
 {
   programs.git.enable = true;
   programs.git.userName = "Jeff Kreeftmeijer";
-  programs.git.userEmail = "jeffkreeftmeijer@gmail.com";
+  programs.git.userEmail = "jeff@kreeft.me";
   programs.git.extraConfig.init.defaultBranch = "main";
   programs.git.ignores = ["DS_Store"];
 }
