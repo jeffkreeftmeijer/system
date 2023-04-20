b2f28405c453f6dc395d5cb09e5d86300a1695e6
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-04-20 11:51:23 +0200

    Add nix-darwin

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +17,7 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, configured-emacs, ... }:
+  outputs = { nixpkgs, darwin, home-manager, configured-emacs, ... }:
     let
       system = "x86_64-darwin";
       pkgs = import nixpkgs {
@@ -22,6 +26,14 @@
         config.allowUnfree = true;
       };
     in {
+      darwinConfigurations."jeff-mbp" = darwin.lib.darwinSystem {
+        inherit system;
+
+        modules = [
+          ./modules/nix-daemon.nix
+        ];
+      };
+
       homeConfigurations.jeffkreeftmeijer = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
   
