3f2d8b7edefb6137835679c162f8e3605014e58a
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-04-25 12:45:13 +0200

    Add git commit and tag signing

--- modules/git.nix
+++ modules/git.nix
@@ -2,6 +2,8 @@
   programs.git.enable = true;
   programs.git.userName = "Jeff Kreeftmeijer";
   programs.git.userEmail = "jeff@kreeft.me";
+  programs.git.signing.key = "0x90A581FD6D796692";
+  programs.git.signing.signByDefault = true;
   programs.git.extraConfig.init.defaultBranch = "main";
   programs.git.extraConfig.github.user = "jeffkreeftmeijer";
   programs.git.ignores = ["DS_Store"];
