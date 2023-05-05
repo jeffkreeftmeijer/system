0bd12bbd5e95b5c11e355e0203da39d47565ff87
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-05 09:01:03 +0200

    Set GitHub username

--- modules/git.nix
+++ modules/git.nix
@@ -3,5 +3,6 @@
   programs.git.userName = "Jeff Kreeftmeijer";
   programs.git.userEmail = "jeff@kreeft.me";
   programs.git.extraConfig.init.defaultBranch = "main";
+  programs.git.extraConfig.github.user = "jeffkreeftmeijer";
   programs.git.ignores = ["DS_Store"];
 }
