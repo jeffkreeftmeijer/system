158e4e71cc85d392fe5ba428ec2082d4dfc7b956
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-24 13:15:04 +0100

    Show hidden files in Finder

--- flake.nix
+++ flake.nix
@@ -18,14 +18,19 @@
 
       environment.variables.EDITOR = "vim";
 
-      system.defaults.dock = {
-        appswitcher-all-displays = true; # Show the app switcher on all displays
-        autohide = true; # Automatically hide the dock
-        autohide-delay = 0.0; # ... immediately
-        autohide-time-modifier = 0.0; # ... and instantly
-        launchanim = false; # No launch animations
-        static-only = true; # Only show open apps in the Dock
-        tilesize = 64; # Reset the Dock icon size to 64
+      system.defaults = {
+        NSGlobalDomain = {
+          AppleShowAllFiles = true; # Show hidden files in Finder
+        };
+        dock = {
+          appswitcher-all-displays = true; # Show the app switcher on all displays
+          autohide = true; # Automatically hide the dock
+          autohide-delay = 0.0; # ... immediately
+          autohide-time-modifier = 0.0; # ... and instantly
+          launchanim = false; # No launch animations
+          static-only = true; # Only show open apps in the Dock
+          tilesize = 64; # Reset the Dock icon size to 64
+        };
       };
 
       # Auto upgrade nix package and the daemon service.
