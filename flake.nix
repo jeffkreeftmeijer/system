84073a5060aac943434dfb79d216853aedcb6cdb
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-24 11:21:31 +0100

    Set $EDITOR to vim

--- flake.nix
+++ flake.nix
@@ -16,6 +16,8 @@
         [ pkgs.vim
         ];
 
+      environment.variables.EDITOR = "vim";
+
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
