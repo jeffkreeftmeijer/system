d70586d50ba7e4c129a109a83a1f67bed13687cf
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-24 12:42:16 +0100

    Import Dock settings

--- flake.nix
+++ flake.nix
@@ -18,6 +18,16 @@
 
       environment.variables.EDITOR = "vim";
 
+      system.defaults.dock = {
+        appswitcher-all-displays = true; # Show the app switcher on all displays
+        autohide = true; # Automatically hide the dock
+        autohide-delay = 0.0; # ... immediately
+        autohide-time-modifier = 0.0; # ... and instantly
+        launchanim = false; # No launch animations
+        static-only = true; # Only show open apps in the Dock
+        tilesize = 64; # Reset the Dock icon size to 64
+      };
+
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
