e69cebfc28816a183a4345c852915bcf19bb5ced
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-25 14:00:31 +0100

    Remap caps lock to escape

--- flake.nix
+++ flake.nix
@@ -64,20 +64,26 @@
 
       environment.variables.EDITOR = "vim";
 
-      system.defaults = {
-        NSGlobalDomain = {
-          AppleShowAllFiles = true; # Show hidden files in Finder
-          AppleShowAllExtensions = true; # Show file extensions in Finder
-          "com.apple.sound.beep.volume" = 0.000;
+      system = {
+        defaults = {
+          NSGlobalDomain = {
+            AppleShowAllFiles = true; # Show hidden files in Finder
+            AppleShowAllExtensions = true; # Show file extensions in Finder
+            "com.apple.sound.beep.volume" = 0.000;
+          };
+          dock = {
+            appswitcher-all-displays = true; # Show the app switcher on all displays
+            autohide = true; # Automatically hide the dock
+            autohide-delay = 0.0; # ... immediately
+            autohide-time-modifier = 0.0; # ... and instantly
+            launchanim = false; # No launch animations
+            static-only = true; # Only show open apps in the Dock
+            tilesize = 64; # Reset the Dock icon size to 64
+          };
         };
-        dock = {
-          appswitcher-all-displays = true; # Show the app switcher on all displays
-          autohide = true; # Automatically hide the dock
-          autohide-delay = 0.0; # ... immediately
-          autohide-time-modifier = 0.0; # ... and instantly
-          launchanim = false; # No launch animations
-          static-only = true; # Only show open apps in the Dock
-          tilesize = 64; # Reset the Dock icon size to 64
+        keyboard = {
+          enableKeyMapping = true;
+          remapCapsLockToEscape = true;
         };
       };
 
