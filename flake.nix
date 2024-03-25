da2b6d8a80f2e87f58b2e3eb742745f41333985d
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-25 09:42:20 +0100

    Add git, import configuration

--- flake.nix
+++ flake.nix
@@ -139,6 +139,15 @@
             home.username = "jeff";
             home.homeDirectory = "/Users/jeff";
             home.stateVersion = "23.11";
+
+            programs.git.enable = true;
+            programs.git.userName = "Jeff Kreeftmeijer";
+            programs.git.userEmail = "jeff@kreeft.me";
+            programs.git.signing.key = "0x90A581FD6D796692";
+            programs.git.signing.signByDefault = true;
+            programs.git.extraConfig.init.defaultBranch = "main";
+            programs.git.extraConfig.github.user = "jeffkreeftmeijer";
+            programs.git.ignores = ["DS_Store" ".envrc"];
           };
         }
       ];
