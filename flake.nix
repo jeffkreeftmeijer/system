31deb28fb476a8ec9c740da46a7026bb669daca1
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-03 18:59:57 +0200

    Show week numbers in iCal

--- flake.nix
+++ flake.nix
@@ -165,6 +165,7 @@
               "com.apple.Safari".IncludeDevelopMenu = true;
               "com.apple.Safari".ShowFullURLInSmartSearchField = true;
               "com.apple.Safari".AutoFillPasswords = true;
+              "com.apple.iCal"."Show Week Numbers" = true;
             };
 
             programs.atuin.enable = true;
