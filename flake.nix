f0dc8cfd4d54bc5014f061e2a89c424530cc19dc
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-10 00:42:41 +0200

    Set wallpaper to Space Gray Pro.png

--- flake.nix
+++ flake.nix
@@ -102,6 +102,10 @@
           enableKeyMapping = true;
           remapCapsLockToEscape = true;
         };
+
+        activationScripts.postUserActivation.text = ''
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"/System/Library/Desktop Pictures/Solid Colors/Space Gray Pro.png\" as POSIX file"
+        '';
       };
 
       # Auto upgrade nix package and the daemon service.
