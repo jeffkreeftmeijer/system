f9fd0d0e4a175e7860ea0c30a1a49365963863bf
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-12 14:14:28 +0200

    Move dock defaults to macos module

--- configuration.nix
+++ configuration.nix
@@ -12,15 +12,6 @@
         InitialKeyRepeat = 15;
         KeyRepeat = 2;
       };
-      dock = {
-        appswitcher-all-displays = true; # Show the app switcher on all displays
-        autohide = true; # Automatically hide the dock
-        autohide-delay = 0.0; # ... immediately
-        autohide-time-modifier = 0.0; # ... and instantly
-        launchanim = false; # No launch animations
-        static-only = true; # Only show open apps in the Dock
-        tilesize = 64; # Reset the Dock icon size to 64
-      };
     };
     keyboard = {
       enableKeyMapping = true;
--- modules/macos.nix
+++ modules/macos.nix
@@ -4,5 +4,14 @@
     "com.apple.Safari".ShowFullURLInSmartSearchField = true;
     "com.apple.Safari".AutoFillPasswords = true;
     "com.apple.iCal"."Show Week Numbers" = true;
+    "com.apple.Dock" = {
+      appswitcher-all-displays = true; # Show the app switcher on all displays
+      autohide = true; # Automatically hide the dock
+      autohide-delay = 0.0; # ... immediately
+      autohide-time-modifier = 0.0; # ... and instantly
+      launchanim = false; # No launch animations
+      static-only = true; # Only show open apps in the Dock
+      tilesize = 64; # Reset the Dock icon size to 64
+    };
   };
 }
