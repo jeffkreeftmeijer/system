b3525a5dd5477b80cfa916b326ab788f8bd0140a
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-12 14:30:01 +0200

    Clean up macos.nix

--- modules/macos.nix
+++ modules/macos.nix
@@ -1,10 +1,6 @@
 {
   home-manager.users.jeff.targets.darwin.defaults = {
-    "com.apple.Safari".IncludeDevelopMenu = true;
-    "com.apple.Safari".ShowFullURLInSmartSearchField = true;
-    "com.apple.Safari".AutoFillPasswords = true;
-    "com.apple.iCal"."Show Week Numbers" = true;
-    "com.apple.Dock" = {
+    "com.apple.dock" = {
       appswitcher-all-displays = true; # Show the app switcher on all displays
       autohide = true; # Automatically hide the dock
       autohide-delay = 0.0; # ... immediately
@@ -12,6 +8,14 @@
       launchanim = false; # No launch animations
       static-only = true; # Only show open apps in the Dock
       tilesize = 64; # Reset the Dock icon size to 64
+    };
+    "com.apple.ical" = {
+      "Show Week Numbers" = true;
+    };
+    "com.apple.safari" = {
+      IncludeDevelopMenu = true;
+      ShowFullURLInSmartSearchField = true;
+      AutoFillPasswords = true;
     };
   };
 }
