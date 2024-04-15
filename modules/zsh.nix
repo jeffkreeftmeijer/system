101b50338d1b36bce3eafd2b8e6bcfa230ca0b4b
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-15 20:53:37 +0200

    Use zsh as default shell

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,5 +1,8 @@
+{ pkgs, ... }:
+
 {
   programs.zsh.enable = true;
+  users.users.jeff.shell = pkgs.zsh;
   home-manager.users.jeff.programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
