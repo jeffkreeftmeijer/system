fcb971e23f513475e2052066f08d504d67568414
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-16 13:05:36 +0200

    Add computerName and hostName

--- machines/macos/configuration.nix
+++ machines/macos/configuration.nix
@@ -6,6 +6,11 @@
     description = "Jeff Kreeftmeijer";
   };
 
+  networking = {
+    computerName = "Jeff’s Laptop";
+    hostName = "macos";
+  };
+
   services.nix-daemon.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
   system.stateVersion = 4;
