30a8c27a423aa08f825ad9a6456fec91169f1dbe
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-16 13:10:10 +0200

    Set time.timeZone

--- machines/macos/configuration.nix
+++ machines/macos/configuration.nix
@@ -11,6 +11,8 @@
     hostName = "macos";
   };
 
+  time.timeZone = "Europe/Amsterdam";
+
   services.nix-daemon.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
   system.stateVersion = 4;
