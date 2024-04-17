2221a1e4efbf7672924a85689688d46b3ba1bcec
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-17 08:00:41 +0200

    Add ble.sh
    As per https://github.com/nix-community/home-manager/pull/3238/files#diff-af5efbbf321d3ea8a4e838dc99965804b72ed0b626c564b0ecfe00994c48add2R116-R122

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,3 +1,14 @@
+{ pkgs, lib, ... }:
+
 {
-  home-manager.users.jeff.programs.bash.enable = true;
+  home-manager.users.jeff.home.packages = [ pkgs.blesh ];
+  home-manager.users.jeff.programs.bash = {
+    enable = true;
+    bashrcExtra = lib.mkBefore ''
+      [[ $- == *i* ]] && source '${pkgs.blesh}/share/blesh/ble.sh' --attach=none
+    '';
+    initExtra = lib.mkAfter ''
+      [[ ''${BLE_VERSION-} ]] && ble-attach
+    '';
+  };
 }
