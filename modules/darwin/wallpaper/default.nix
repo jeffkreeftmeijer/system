4b21cf2827261eec045cabdae47960dd9eef5410
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-18 19:59:37 +0200

    Fix wallpaper path

--- modules/darwin/wallpaper/default.nix
+++ modules/darwin/wallpaper/default.nix
@@ -3,7 +3,7 @@
     osascript -e "
       tell application \"System Events\"
         tell every desktop
-          set picture to \"~/.config/nix/wallpaper/dark.png\"
+          set picture to \"~/.config/nix/modules/darwin/wallpaper/dark.png\"
         end tell
       end tell"
   '';
