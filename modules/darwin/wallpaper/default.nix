38c6d89865a066bcfa04a8c2d1648075df915d37
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-18 20:13:57 +0200

    Add win95.png, use that as wallpaper

--- modules/darwin/wallpaper/default.nix
+++ modules/darwin/wallpaper/default.nix
@@ -3,7 +3,7 @@
     osascript -e "
       tell application \"System Events\"
         tell every desktop
-          set picture to \"~/.config/nix/modules/darwin/wallpaper/dark.png\"
+          set picture to \"~/.config/nix/modules/darwin/wallpaper/win95.png\"
         end tell
       end tell"
   '';
