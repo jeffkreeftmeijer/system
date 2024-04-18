39ba0d974addd48ee6667ce1000146a153b1a7c3
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-18 17:02:00 +0200

    Add Eat integration for zsh and bash

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,4 +1,14 @@
+{ lib, ... } :
+
 {
   home-manager.users.jeff.programs.emacs.enable = true;
   services.emacs.enable = true;
+  home-manager.users.jeff.programs.zsh.initExtra = lib.mkAfter ''
+    [ -n "$EAT_SHELL_INTEGRATION_DIR" ] && \
+      source "$EAT_SHELL_INTEGRATION_DIR/zsh"
+  '';
+  home-manager.users.jeff.programs.bash.initExtra = lib.mkAfter ''
+    [ -n "$EAT_SHELL_INTEGRATION_DIR" ] && \
+      source "$EAT_SHELL_INTEGRATION_DIR/bash"
+  '';
 }
