bf8ed19e4e6dac34721e7ed43145d08567daad61
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-19 08:59:58 +0200

    Add mbsync, add imap configuration from kreeft.me

--- modules/notmuch.nix
+++ modules/notmuch.nix
@@ -1,12 +1,18 @@
 {
   home-manager.users.jeff = {
     programs.notmuch.enable = true;
+    programs.mbsync.enable = true;
 
     accounts.email.accounts.kreeft = {
       address = "jeff@kreeft.me";
+      userName = "jeff@kreeft.me";
       realName = "Jeff Kreeftmeijer";
+      imap.host = "imap.transip.email";
+      passwordCommand =
+        "security find-generic-password -w -a jeff@kreeft.me -s imap.transip.email";
       primary = true;
       notmuch.enable = true;
+      mbsync.enable = true;
     };
   };
 }
