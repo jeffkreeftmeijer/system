c939cbf025826246166b0431c63894f15e25b9a2
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-21 01:18:14 +0200

    Update Git signing key to new key ID

--- modules/git.nix
+++ modules/git.nix
@@ -3,7 +3,7 @@
     enable = true;
     userName = "Jeff Kreeftmeijer";
     userEmail = "jeff@kreeft.me";
-    signing.key = "0x90A581FD6D796692";
+    signing.key = "0xDBE78FAED96BF0D0";
     signing.signByDefault = true;
     extraConfig.init.defaultBranch = "main";
     extraConfig.github.user = "jeffkreeftmeijer";
