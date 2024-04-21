7c0db328032792c97cffeeeda2b3618efa9c1394
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-22 00:15:51 +0200

    Configure yabai paddings and window gap

--- modules/darwin/yabai.nix
+++ modules/darwin/yabai.nix
@@ -2,6 +2,13 @@
   services.yabai = {
     enable = true;
     enableScriptingAddition = true;
-    config = { layout = "bsp"; };
+    config = {
+      layout = "bsp";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
+    };
   };
 }
