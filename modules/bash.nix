2fc5c9518bb96a8e4f62177a0ab918b9ee53fb44
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-01 20:08:46 +0200

    Move all blesh configuration to blesh.nix

--- modules/bash.nix
+++ modules/bash.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 
 {
-  home-manager.users.jeff.home.packages = [ pkgs.blesh ];
   home-manager.users.jeff.programs.bash.enable = true;
 }
--- modules/blesh.nix
+++ modules/blesh.nix
@@ -1,12 +1,15 @@
 { pkgs, lib, ... }:
 
 {
-  home-manager.users.jeff.programs.bash = {
-    bashrcExtra = lib.mkBefore ''
-      [[ $- == *i* ]] && source '${pkgs.blesh}/share/blesh/ble.sh' --attach=none
-    '';
-    initExtra = lib.mkAfter ''
-      [[ ''${BLE_VERSION-} ]] && ble-attach
-    '';
+  home-manager.users.jeff = {
+    home.packages = [ pkgs.blesh ];
+    programs.bash = {
+      bashrcExtra = lib.mkBefore ''
+        [[ $- == *i* ]] && source '${pkgs.blesh}/share/blesh/ble.sh' --attach=none
+      '';
+      initExtra = lib.mkAfter ''
+        [[ ''${BLE_VERSION-} ]] && ble-attach
+      '';
+    };
   };
 }
