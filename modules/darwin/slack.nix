e3d410940fe7668b5a29f1c920ec956555a523b3
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-05-16 11:09:55 +0200

    Disable Slack updates
    As per https://slack.com/help/articles/360035635174-Deploy-Slack-for-macOS

--- modules/darwin/slack.nix
+++ modules/darwin/slack.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... } :
+{ pkgs, ... }:
 
 {
   home-manager.users.jeff.home.packages = [ pkgs.slack ];
+  system.activationScripts.postUserActivation.text = ''
+    defaults write ~/Library/Preferences/com.tinyspeck.slackmacgap.plist SlackNoAutoUpdates -bool YES
+  '';
 }
