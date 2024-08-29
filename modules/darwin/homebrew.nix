550f76f11230184c47f9c6160f25fa904793c111
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-08-29 10:16:39 +0200

    Manually add homebrew to shell env on zsh

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,3 +1,12 @@
 {
-  homebrew.onActivation.cleanup = "zap";
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+  };
+
+  home-manager.users.jeff.programs.zsh = {
+    initExtra = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
+  };
 }
