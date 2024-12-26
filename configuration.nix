02189ccc877a5980632bfe9c53e4bbf354c1b19e
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-26 15:17:02 +0000

    Add apple-silicon-support yo configuration imports

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./apple-silicon-support
     ];
 
   # Use the systemd-boot EFI boot loader.
