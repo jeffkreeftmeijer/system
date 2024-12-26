b91f8d75adfa196a613ec2a9da598913fb55b253
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-26 15:17:59 +0000

    Turn off canTouchEfiVariables

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.canTouchEfiVariables = false;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
