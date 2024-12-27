5b2528e29a608d597a31515e8ae5161b05f30ece
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-27 15:24:15 +0000

    Use pinentry-gnome3 as pinentryPackage

--- modules/gnupg.nix
+++ modules/gnupg.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
+
 {
   home-manager.users.jeff.programs.gpg.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 }
