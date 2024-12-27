ff664ec6bd183dccb9796184f1dbca5f44c3a4c0
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-27 18:08:48 +0000

    Add gnome settings for sounds and interface

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -2,4 +2,17 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  programs.dconf.enable = true;
+
+  home-manager.users.jeff.dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      show-battery-percentage = true;
+    };
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+    };
+  };
 }
