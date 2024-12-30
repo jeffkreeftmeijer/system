c6e23ae239ccdee749a8a4ec14da798d57ad3e3e
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-30 10:03:59 +0100

    Use autosuggestion.enable instead of enableAutosuggestions in zsh module

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... } :
+{ pkgs, ... }:
 
 {
   programs.zsh.enable = true;
   users.users.jeff.shell = pkgs.zsh;
   home-manager.users.jeff.programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
   };
 }
