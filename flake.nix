617dc8b1f0f0c56554090b89bd2fbc7e7e1fa6d5
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-12-30 10:10:20 +0100

    Bump nixpkgs and home-manager to 24.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Jeff's system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew = {
